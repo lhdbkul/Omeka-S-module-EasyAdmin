87dca809f29c593e6b7e967223017586ab4cf637
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-09 00:00:00 +0000

    Managed deduplication with annotations.

--- src/Job/DbValueClean.php
+++ src/Job/DbValueClean.php
@@ -345,7 +345,7 @@ class DbValueClean extends AbstractCheck
 
         if ($resourceIds) {
             // For specified values.
-            $sqlWhere1 = 'WHERE `resource_id` IN (:resource_ids)';
+            $sqlWhere1 = 'WHERE `v`.`resource_id` IN (:resource_ids)';
             $sqlWhere2 = 'AND `resource_id` IN (:resource_ids)';
             $bind['resource_ids'] = $resourceIds;
             $types['resource_ids'] = \Doctrine\DBAL\Connection::PARAM_INT_ARRAY;
@@ -355,25 +355,40 @@ class DbValueClean extends AbstractCheck
             $sqlWhere2 = '';
         }
 
-        // Use MIN(id) to get one id per group of duplicates.
-        // This is standard SQL that works with ONLY_FULL_GROUP_BY enabled,
-        // avoiding the need to modify sql_mode which could affect other queries.
-        // For deduplication, keeping the value with the lowest id is reasonable.
+        // Use MIN(id) to get one id per group of duplicates. This is standard
+        // SQL that works with ONLY_FULL_GROUP_BY enabled, avoiding the need to
+        // modify sql_mode. Keeping the value with the lowest id is reasonable.
 
-        // Drop temporary table if it exists from a previous failed run.
-        $this->connection->executeStatement('DROP TABLE IF EXISTS `value_temporary`');
+        // Drop temporary table if it exists from a previous run.
+        $this->connection->executeStatement(
+            'DROP TABLE IF EXISTS `value_temporary`'
+        );
 
         // Create temporary table with one id per unique value combination.
-        // Include value_annotation_id so values with different annotations are
-        // not duplicates.
+        // Include value_annotation_id so values with different annotations
+        // are not duplicates. When module Annotate is installed, also include
+        // annotation_value.field and ordinal so body/target values are not
+        // deduplicated.
+        $hasAnnotate = (bool) $this->connection
+            ->executeQuery(
+                "SHOW TABLES LIKE 'annotation_value'"
+            )->fetchOne();
+        if ($hasAnnotate) {
+            $sqlJoin = 'LEFT JOIN `annotation_value` AS `av` ON `av`.`id` = `v`.`id`';
+            $sqlGroupExtra = ', `av`.`field`, `av`.`ordinal`';
+        } else {
+            $sqlJoin = '';
+            $sqlGroupExtra = '';
+        }
         try {
             $sqlCreate = <<<SQL
                 CREATE TEMPORARY TABLE `value_temporary` (`id` INT, PRIMARY KEY (`id`))
                 AS
-                    SELECT MIN(`id`) AS `id`
-                    FROM `value`
+                    SELECT MIN(`v`.`id`) AS `id`
+                    FROM `value` AS `v`
+                    $sqlJoin
                     $sqlWhere1
-                    GROUP BY `resource_id`, `property_id`, `value_resource_id`, `type`, `lang`, `value`, `uri`, `is_public`, `value_annotation_id`
+                    GROUP BY `v`.`resource_id`, `v`.`property_id`, `v`.`value_resource_id`, `v`.`type`, `v`.`lang`, `v`.`value`, `v`.`uri`, `v`.`is_public`, `v`.`value_annotation_id` $sqlGroupExtra
                 SQL;
             $this->connection->executeStatement($sqlCreate, $bind, $types);
 
